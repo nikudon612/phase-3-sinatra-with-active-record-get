8709eb41e0a198393688ebc650ceb2893da2b21f
Nick Bechtel <bechtel.nick@gmail.com>
2021-12-03 18:16:25 -0500

    commit tree 3d4df6f7e6aaf1b694b47d260e93533ec7407066

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,31 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  get '/games' do
+    # get all the games from the database
+    games = Game.all.order(:title).limit(10)
 
-  get '/' do
-    { message: "Hello world" }.to_json
+    # return a JSON response with an array of all the game data
+    games.to_json
   end
 
+  # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+
+    # send a JSON-formatted response of the game data
+    game.to_json(
+      only: %i[id title genre price],
+      include: {
+        reviews: {
+          only: %i[comment score],
+          include: {
+            user: {
+              only: [:name],
+            },
+          },
+        },
+      },
+    )
+  end
 end
